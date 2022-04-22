39349128195ad9ee8f12f45aa23bdf2a42068917
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-04-22 13:28:41 -0400

    added a connecting and connection confirmation message

---
+++ client.js
@@ -0,0 +1,31 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '192.168.1.159',// IP address here,
+    port: 50541// PORT number here,
+  });
+
+   // interpret incoming data as text
+   conn.setEncoding("utf8");
+
+  
+  conn.on('connect', () => {
+
+    console.log("Connecting ...");
+    conn.write('Name: OAR');
+    console.log("Successfully connected to game server");
+  })
+  
+  //this is to detect what user is inputting
+  
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+ 
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,26 +1,6 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.1.159',// IP address here,
-    port: 50541// PORT number here,
-  });
-
-   // interpret incoming data as text
-   conn.setEncoding("utf8");
-
-  console.log("Connecting ...");
-  
-  //this is to detect what user is inputting
-  
-  conn.on('data', (data) => {
-    console.log(data);
-  });
- 
-
-  return conn;
-};
+const { connect } = require('./client');
 
 
 connect();
