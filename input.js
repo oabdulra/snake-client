e8f2aae16bb0e76e7e04973b0a871250671b8321
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-04-22 16:38:14 -0400

    added new input file to handle all the input for snake game

---
+++ input.js
@@ -0,0 +1,15 @@
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  
+};
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,7 +1,7 @@
-const net = require("net");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const { connect } = require('./client');
-
-
+console.log("Connecting ...");
 connect();
 
+setupInput();
